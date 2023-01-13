ac7751e6c0c4fe3893505895e822800213980802
Gumen Anton <gumen.tosha@yandex.ru>
2023-01-13 14:11:13 +0300

    Sem4

---
+++ task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Будет создан массив длиной A в диапозоне 0-A");
+Console.Write("Введите любое число (A): ");
+
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[enterNumber];
+
+for (int i = 0; i < enterNumber; i++)
+{
+    array[i] = new Random().Next(enterNumber + 1);
+   
+}
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
